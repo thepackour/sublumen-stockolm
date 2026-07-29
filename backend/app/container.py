e7766eea29a97e89cb173e1285851da86ef974d5
thepackour <thepackour5610@gmail.com>
2026-07-29 20:27:30 +0900

    feat: 서비스 일부 추가 + StockRepository SessionLocal 사용하도록 변경

--- backend/app/container.py
+++ backend/app/container.py
@@ -1,5 +1,5 @@
 from app.clients.fdr_client import StockSymbolService
-from app.clients.gemini_embedding import GeminiEmbeddingClient
+from app.clients.gemini_embedding import EmbeddingClient
 from app.clients.news_client import NewsClient
 from app.core.database import SessionLocal
 from app.repositories.news_keyword_repository import NewsKeywordRepository
@@ -7,16 +7,14 @@ from app.repositories.postgres_news_embedding_repository import NewsEmbeddingRep
 from app.repositories.postgres_news_repository import NewsRepository
 from app.repositories.postgres_stock_repository import StockRepository
 from app.services.news_collect_service import NewsCollectService
+from app.services.news_embedding_service import NewsEmbeddingService
 from app.services.news_query_service import NewsQueryService
+from app.services.stock_collect_service import StockCollectService
 
 
 class Container:
 
     def __init__(self):
-        # clients
-        self.stock_symbol_service = StockSymbolService()
-        self.news_client = NewsClient()
-        self.embedding_client = GeminiEmbeddingClient()
 
         # repositories
         self.news_repository = NewsRepository(SessionLocal)
@@ -24,7 +22,19 @@ class Container:
         self.news_keyword_repository = NewsKeywordRepository(SessionLocal)
         self.stock_repository = StockRepository(SessionLocal)
 
+        # clients
+        self.stock_symbol_service = StockSymbolService()
+        self.news_client = NewsClient()
+        self.embedding_client = EmbeddingClient()
+
         # services
+        self.news_embedding_service = NewsEmbeddingService(
+            self.embedding_client
+        )
+        self.stock_collect_service = StockCollectService(
+            self.stock_repository,
+            self.stock_symbol_service,
+        )
         self.news_query_service = NewsQueryService(
             self.news_repository,
             self.news_embedding_repository,
@@ -37,11 +47,11 @@ class Container:
             self.news_keyword_repository,
             self.news_client,
             self.stock_symbol_service,
-            self.embedding_client
+            self.news_embedding_service
         )
 
     def initialize(self):
-        self.stock_symbol_service.initialize()
+        self.stock_collect_service.initialize()
         self.news_keyword_repository.initialize()
 
     def shutdown(self):

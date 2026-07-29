f0b5da1291f9f0d9efe120ac34a637fd5933597f
thepackour <thepackour5610@gmail.com>
2026-07-29 15:22:14 +0900

    feat: NEWS200_2 추가

--- backend/app/core/success_code.py
+++ backend/app/core/success_code.py
@@ -3,6 +3,7 @@ from enum import Enum
 class SuccessCode(Enum):
 
     NEWS200_1 = (200, "NEWS200_1", "뉴스 조회에 성공했습니다.")
+    NEWS200_2 = (200, "NEWS200_2", "뉴스 수집에 성공했습니다.")
 
     ANALYSIS200_1 = (200, "ANALYSIS200_1", "분석 조회에 성공했습니다.")
     ANALYSIS201_1 = (201, "ANALYSIS201_1", "분석 결과가 성공적으로 생성되었습니다.")

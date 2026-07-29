385fd5c5bdd02e25a3270ae74236fa4b7d719936
thepackour <thepackour5610@gmail.com>
2026-07-29 20:27:48 +0900

    fix: StockRepository SessionLocal 사용하도록 변경

--- backend/app/repositories/postgres_stock_repository.py
+++ backend/app/repositories/postgres_stock_repository.py
@@ -1,51 +1,51 @@
 from datetime import datetime
-
-from fastapi import Depends
+from typing import Optional
 
 from sqlalchemy import select
-from sqlalchemy.orm import Session
 
 from app.schemas.stock import Stock
-from app.core.database import get_db
 
 
 class StockRepository:
 
-    def __init__(self, db: Session):
-        self.db = db
+    def __init__(self, session_factory):
+        self.session_factory = session_factory
 
     def save(self, stock: Stock):
-        self.db.add(stock)
-        self.db.commit()
-        self.db.refresh(stock)
-        return stock
+        with self.session_factory() as db:
+            db.add(stock)
+            db.commit()
+            db.refresh(stock)
+            return stock
 
     def save_all(self, stocks: list[Stock]):
-        self.db.add_all(stocks)
-        self.db.commit()
-        self.db.refresh(stocks)
-        return stocks
-
-        return self.db.scalars(stmt).all()
+        with self.session_factory() as db:
+            db.add_all(stocks)
+            db.commit()
+            db.refresh(stocks)
+            return stocks
 
     def find_all(self):
         stmt = select(Stock)
+        with self.session_factory() as db:
+            return db.scalars(stmt).all()
 
-        return self.db.scalars(stmt).all()
+    def find_by_symbol(self, symbol: str) -> Optional[Stock]:
+        with self.session_factory() as db:
+            stmt = (
+                select(Stock)
+                .where(Stock.symbol == symbol)
+            )
+            return db.scalars(stmt).one()
 
     def delete(self, stock: Stock, hard_delete: bool | None = False):
-        if hard_delete:
-            self.db.delete(stock)
-            self.db.commit()
-        else:
-            stmt = select(Stock).where(Stock.id == stock.id)
-            res = self.db.execute(stmt)
-            res.deleted_at = datetime.now()
-            self.db.commit()
-            self.db.refresh(stock)
-
-
-def get_stock_repository(
-    db: Session = Depends(get_db),
-):
-    return StockRepository(db)
+        with self.session_factory() as db:
+            if hard_delete:
+                db.delete(stock)
+                db.commit()
+            else:
+                stmt = select(Stock).where(Stock.id == stock.id)
+                res = db.execute(stmt)
+                res.deleted_at = datetime.now()
+                db.commit()
+                db.refresh(stock)

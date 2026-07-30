7c452f3e40f490efaabd9a1ac534bd4609657d28
thepackour <thepackour5610@gmail.com>
2026-07-30 15:34:53 +0900

    fix: 타입 포맷 매칭 수정

--- backend/app/core/response.py
+++ backend/app/core/response.py
@@ -1,24 +1,38 @@
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
 from app.core.error_code import ErrorCode
+from app.core.logger import logger
 from app.core.success_code import SuccessCode
 
-def success(code: SuccessCode, data=None):
+def success(code: SuccessCode, data = None):
+    logger.info(
+        "API 응답 (%s): %s (%s)",
+        code.code,
+        code.message,
+        "" if data is None else str(data),
+    )
     return JSONResponse(
         status_code=code.status,
-        content={
+        content=jsonable_encoder({
             "status": code.status,
             "message": code.message,
             "data": data
-        }
+        })
     )
 
-def error(code: ErrorCode, data=None):
+def error(code: ErrorCode, data = None):
+    logger.warning(
+        "API 응답 (%d): %s (%s)",
+        code.code,
+        code.message,
+        "" if data is None else str(data),
+    )
     return JSONResponse(
         status_code=code.status,
-        content={
+        content=jsonable_encoder({
             "status": code.status,
             "message": code.message,
             "data": data
-        }
+        })
     )

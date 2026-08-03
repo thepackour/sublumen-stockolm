5dd49791e3ca6298ef081ee08243d33442316a37
thepackour <thepackour5610@gmail.com>
2026-08-03 23:44:05 +0900

    fix: 로직 오류 수정 StockListing에 "KRX-DESC" 입력. 일부 메소드 반환형 변경 및 return 값 수정

--- backend/app/clients/fdr_client.py
+++ backend/app/clients/fdr_client.py
@@ -2,15 +2,16 @@ from typing import Optional
 
 import FinanceDataReader as fdr
 import pandas as pd
+from pandas import DataFrame
 
 
 class StockSymbolService:
 
     def __init__(self):
-        self.stocks = None
+        self.stocks: Optional[DataFrame] = None
 
     def initialize(self):
-        krx = fdr.StockListing("KRX").rename(columns={"Code": "Symbol"})
+        krx = fdr.StockListing("KRX-DESC").rename(columns={"Code": "Symbol"})
         nasdaq = fdr.StockListing("NASDAQ")
 
         self.stocks = pd.concat([krx, nasdaq], ignore_index=True)
@@ -28,10 +29,10 @@ class StockSymbolService:
             self.stocks["Name"].str.contains(keyword, case=False, na=False)
         ]
 
-        return result[:limit]
+        return result[:limit].to_dict("records")
 
-    def get_stock(self, symbol: str):
-        return self.stocks[self.stocks["Symbol"] == symbol]
+    def get_stock(self, symbol: str) -> dict:
+        return self.stocks[self.stocks["Symbol"] == symbol].iloc[0].to_dict()
 
-    def get_stock_by_stock_id(self, stock_id: int):
-        return self.stocks[self.stocks["StockId"] == stock_id]
+    def get_stock_by_stock_id(self, stock_id: int) -> dict:
+        return self.stocks[self.stocks["StockId"] == stock_id].iloc[0].to_dict()

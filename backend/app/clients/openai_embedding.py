81f288c299bd20c375c5ba1f6a6473ef1bdd7b40
thepackour <thepackour5610@gmail.com>
2026-08-05 17:34:58 +0900

    feat: openai client 구현

---
+++ backend/app/clients/openai_embedding.py
@@ -0,0 +1,32 @@
+from openai import OpenAI
+
+from app.core.config import settings
+from app.core.logger import logger
+
+
+class EmbeddingClient:
+
+    def __init__(self):
+        self.client = OpenAI(api_key=settings.OPENAI_API_KEY)
+        self.model = "text-embedding-3-small"
+
+
+    def embed_chunks(self, chunks: list[str]) -> list[list[float]]:
+        res = self.client.embeddings.create(
+            model=self.model,
+            input=chunks
+        )
+        logger.info(
+            "임베딩 API: 청크 %d개 생성",
+            len(res.data),
+        )
+        return [item.embedding for item in res.data]
+
+
+    def embed_keyword(self, keyword: str) -> list[float]:
+        res = self.client.embeddings.create(
+            model=self.model,
+            input=keyword
+        )
+        logger.info("임베딩 API: 키워드 1개 성공")
+        return res.data[0].embedding

b321a255152f6cf438fba95c02998a56fd14d678
thepackour <thepackour5610@gmail.com>
2026-08-13 14:25:02 +0900

    fix: 반환형 수정 및 deprecated 처리

--- backend/app/services/stock_search_service.py
+++ backend/app/services/stock_search_service.py
@@ -1,10 +1,11 @@
-from typing import Optional
+from warnings import deprecated
 
 from app.core.error_code import ErrorCode
 from app.core.exceptions import ProjectException
 from app.repositories.postgres_stock_repository import StockRepository
 
 
+@deprecated("StockRepository의 search_stocks_by_keyword()를 사용하세요.")
 class StockSearchService:
 
     def __init__(
@@ -13,10 +14,20 @@ class StockSearchService:
     ):
         self.stock_repository = stock_repository
 
-    def find_symbol(self, keyword: str) -> Optional[str]:
-        result = self.stock_repository.search_stocks_by_keyword(keyword)
+    def find_symbol(self, keyword: str, limit: int = 1) -> list[str]:
+        """
+        키워드와 가장 유사한
+
+        Args:
+            keyword:
+            limit:
+
+        Returns:
+
+        """
+        result = self.stock_repository.search_stocks_by_keyword(keyword, limit)
 
         if len(result) == 0:
             raise ProjectException(ErrorCode.STOCK404_1)
 
-        return result[0].symbol
+        return [stock.symbol for stock in result]
